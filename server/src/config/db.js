66d5b34fcfe0caf4b09a44b9c9d0ec956bc9e952
anshul_c0des <anshuldhariya5@gmail.com>
2025-11-07 20:11:34 +0530

    mongodb-memory

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    let uri = process.env.MONGO_URI;
+    if (process.env.NODE_ENV === "test") {
+      const mongoServer = await MongoMemoryServer.create();
+      uri = mongoServer.getUri();
+    }
+
+    await mongoose.connect(uri);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
